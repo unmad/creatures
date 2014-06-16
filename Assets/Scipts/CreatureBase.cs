c46f64d1869ce800aca9b22e182248bc3f83b404
unmad <unmad@mail.ru>
2014-06-16 10:43:01 +0700

    update 7

---
+++ Assets/Scipts/CreatureBase.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreatureBase : MonoBehaviour {
+	public GameObject corpsePrefab;
+	UI ui;
+	int typeID;
+	int age;
+	int maxAge;
+	int size;
+	int maxSize;
+	public Transform visual;
+	int energy;
+	int maxEnergy;
+	float speed;
+	float maxSpeed;
+	bool isMale;
+	bool alive;
+	bool wantFuck;
+	int hp;
+	int maxHp;
+	
+	float timer;
+	float lastTime;
+	List<Transform> food;
+	List<Transform> enemy;
+	Transform target;
+	CreatureGenerator cg;
+	
+	//Magic
+	
+	public float starving;
+	public float hungry;
+	float minScale;
+	float maxScale;
+	
+	string plTag = "Plant";
+	//string plLyr = "Food";
+	string wayTag = "waypoint";
+	string me = "MeatEater";
+	
+	//Magic End
+
+
+}
--- Assets/Scipts/CreatureGenerator.cs
+++ Assets/Scipts/CreatureGenerator.cs
@@ -2,11 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public class CreatureType : MonoBehaviour {
+	public int id;
+	public bool eatMeat;
+	public bool eatPlant;
+	public float typeScale;
+	public int typeSize;
+	public float minscale;
+	public float maxscale;
+	public float typeSpeed;
+}
+
 public class CreatureGenerator : MonoBehaviour {
 	public GameObject creaturePlantPrefab;
 	public List<GameObject> creaturePlant;
 	UI ui;
 	public bool gen;
+	List<CreatureType> CTypes;
 
 	//Magic
 	public float borderSize = 1f;
@@ -50,44 +62,97 @@ public class CreatureGenerator : MonoBehaviour {
 		}
 	}
 
+	void GrowAt(int type, float x, float y){
+
+		if (CTypes[type].eatPlant && !CTypes[type].eatMeat){ //травоядное
+
+			GameObject p = (GameObject)Instantiate(creaturePlantPrefab,new Vector3(x, y, 0),Quaternion.identity);
+			var cre = p.GetComponent<CreaturePlant>();
+			SetInitVar(p, maxAge, CTypes[type].typeSize, CTypes[type].minscale, CTypes[type].maxscale, type, CTypes[type].typeSpeed);
+			creaturePlant.Add(p);
+		}
+
+		if (CTypes[type].eatPlant && CTypes[type].eatMeat){ //всеядное
+			
+			GameObject p = (GameObject)Instantiate(creaturePlantPrefab,new Vector3(x, y, 0),Quaternion.identity);
+			var cre = p.GetComponent<CreaturePlant>();
+			SetInitVar(p, maxAge, CTypes[type].typeSize, CTypes[type].minscale, CTypes[type].maxscale, type, CTypes[type].typeSpeed);
+			creaturePlant.Add(p);
+		}
+
+		if (!CTypes[type].eatPlant && CTypes[type].eatMeat){ //плотоядное
+			
+			GameObject p = (GameObject)Instantiate(creaturePlantPrefab,new Vector3(x, y, 0),Quaternion.identity);
+			var cre = p.GetComponent<CreaturePlant>();
+			SetInitVar(p, maxAge, CTypes[type].typeSize, CTypes[type].minscale, CTypes[type].maxscale, type, CTypes[type].typeSpeed);
+			creaturePlant.Add(p);
+		}
+
+	}
+
+	void SetInitVar(CreaturePlant cre, int maxAge, int maxSize, float minScale, float maxScale, int typeID, float maxSpeed){	//заменить на общий класс кричера
+		cre.SetMaxAge(maxAge);
+		cre.SetMaxSize(maxSize);
+		cre.SetMinScale(minScale);
+		cre.SetMaxScale(maxScale);
+		cre.SetTypeID(typeID);
+		cre.SetMaxSpeed(maxSpeed);
+	}
+
+	void GenerateTypes(){
+		CTypes = new List<CreatureType>();
+
+		for (int cot = 0; cot < ui.countOfTypes; cot++){
+			CreatureType ct;
+			ct.id = cot;
+			ct.eatMeat = false;
+			ct.eatPlant = true;
+			ct.typeScale = Random.Range(minTypesScale, maxTypesScale);
+			ct.typeSize  = Random.Range(minTypeSize, maxTypeSize);
+			ct.minscale = ct.typeScale * 0.3f;
+			ct.maxscale = ct.typeScale;
+			float typeSizeCoef = 1f - ((float)ct.typeSize / (float)maxTypeSize);
+			ct.typeSpeed = Mathf.Lerp(minSpeedRange, maxSpeedRange, typeSizeCoef);
+
+			CTypes.Add(ct);
+		}
+	}
+
 	void Generate(){
 		var width = (ui.width / 2) - borderSize;
 		var height = (ui.height / 2) - borderSize;
-		int l = ui.countOfTypes * ui.countOfCreature;
 
-		for (int cot = 0; cot < ui.countOfTypes; cot++){
-			float typeScale = Random.Range(minTypesScale, maxTypesScale);
-			int typeSize = Random.Range(minTypeSize, maxTypeSize);
-			float minscale = typeScale * 0.3f;
-			float maxscale = typeScale;
-			float typeSizeCoef = 1f - ((float)typeSize / (float)maxTypeSize);
-			float typeSpeed = Mathf.Lerp(minSpeedRange, maxSpeedRange, typeSizeCoef);
-			//Debug.Log("typeSpeed " + typeSpeed);
+		GenerateTypes();
 
+		for (var cot = 0; cot < CTypes.Count; cot++){
 			for (int coc = 0; coc < ui.countOfCreature; coc++){
-				GameObject p = (GameObject)Instantiate(creaturePlantPrefab,new Vector3(Random.Range(-width, width), Random.Range(-height, height), 0),Quaternion.Euler(new Vector3(90f, 270f, 270f)));
-				var cre = p.GetComponent<CreaturePlant>();
-				cre.SetMaxAge(maxAge);
-				cre.SetMaxSize(typeSize);
-				cre.SetMinScale(minscale);
-				cre.SetMaxScale(maxscale);
-				cre.SetTypeID(cot);
-				cre.SetMaxSpeed(typeSpeed);
-				creaturePlant.Add(p);
+
+				var x = Random.Range(-width, width);
+				var y = Random.Range(-height, height);
+
+				GrowAt(cot, x, y);
 			}
 		}
 		gen = true;
 	}
+
 	void Clear (){
+
 		var waypoints = GameObject.FindGameObjectsWithTag (wayTag);
+
 		foreach (var w in waypoints) {
 			Destroy(w);
 		}
-		creaturePlant.ForEach (Destroy);
-		creaturePlant.Clear ();
+
 		var corpses = GameObject.FindGameObjectsWithTag("Corpse");
+		
 		foreach (var c in corpses) {
 			Destroy(c);
 		}
+
+		creaturePlant.ForEach (Destroy);
+		creaturePlant.Clear ();
+		CTypes.ForEach (Destroy);
+		CTypes.Clear ();
 	}
 }
